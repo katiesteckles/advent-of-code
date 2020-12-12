445bc114c982ac026f140e67bd057074f166a574
katiesteckles <katiesteckles@gmail.com>
2020-12-12 18:21:23 +0000

    Day 12

--- day12_part2.py
+++ day12_part2.py
@@ -7,55 +7,44 @@ test_commands = [['F',10],['N',3], ['F',7],['R',90],['F',11]]
 
 # directions: north = 0, northeast = 1, east = 2, southeast = 3, south = 4, southwest = 5, west = 6, northwest = 7
 
-def parse_command(command,position):
-    current_pos = copy.deepcopy(position)
-    print(current_pos)
+def move_waypoint_and_boat(command,boat_position,rel_waypoint_position):
+    waypoint_relpos = copy.deepcopy(rel_waypoint_position)
+    boat_pos = copy.deepcopy(boat_position)
+    print(boat_pos,waypoint_relpos)
     if command[0] == 'R':
-        print('Turning right ' + str(command[1]))
-        current_pos[2] = int((current_pos[2] + (command[1] / 45)) % 8)
+        print('Rotating the waypoint right ' + str(command[1]))
+        #relative_waypoint_pos = [waypoint_pos[0]-boat_pos[0], waypoint_pos[1]-boat_pos[1]]
+        for i in range(int(command[1]/90)):
+            waypoint_relpos = [-waypoint_relpos[1],waypoint_relpos[0]]
     if command[0] == 'L':
-        print('Turning left ' + str(command[1]))
-        current_pos[2] = int((current_pos[2] - (command[1] / 45)) % 8)
+        print('Rotating the waypoint left ' + str(command[1]))
+        for i in range(int(command[1]/90)):
+            waypoint_relpos = [waypoint_relpos[1],-waypoint_relpos[0]]
     if command[0] == 'N':
-        print('Going north ' + str(command[1]))
-        current_pos[1] -= command[1]
+        print('Moving the waypoint north ' + str(command[1]))
+        waypoint_relpos[1] -= command[1]
     if command[0] == 'S':
-        print('Going south ' + str(command[1]))
-        current_pos[1] += command[1]
+        print('Moving the waypoint south ' + str(command[1]))
+        waypoint_relpos[1] += command[1]
     if command[0] == 'E':
-        print('Going east ' + str(command[1]))
-        current_pos[0] += command[1]
+        print('Moving the waypoint east ' + str(command[1]))
+        waypoint_relpos[0] += command[1]
     if command[0] == 'W':
-        print('Going west ' + str(command[1]))
-        current_pos[0] -= command[1]
+        print('Moving the waypoint west ' + str(command[1]))
+        waypoint_relpos[0] -= command[1]
     if command[0] == 'F':
-        print('Going forward ' + str(command[1]))
-        if current_pos[2] == 0:
-            current_pos[1] -= command[1]
-        if current_pos[2] == 1:
-            current_pos[1] -= command[1]
-            current_pos[0] += command[1]
-        if current_pos[2] == 2:
-            current_pos[0] += command[1]
-        if current_pos[2] == 3:
-            current_pos[1] += command[1]
-            current_pos[0] += command[1]
-        if current_pos[2] == 4:
-            current_pos[1] += command[1]
-        if current_pos[2] == 5:
-            current_pos[1] += command[1]
-            current_pos[0] -= command[1]
-        if current_pos[2] == 6:
-            current_pos[0] -= command[1]
-        if current_pos[2] == 7:
-            current_pos[1] -= command[1]
-            current_pos[0] -= command[1]
-    return current_pos
+        print('Going to the waypoint ' + str(command[1]) + ' times')
+        amount_of_move_x = waypoint_relpos[0] * command[1]
+        amount_of_move_y = waypoint_relpos[1] * command[1]
+        #waypoint_pos = [waypoint_pos[0] + amount_of_move_x, waypoint_pos[1] + amount_of_move_y]
+        boat_pos = [boat_pos[0] + amount_of_move_x, boat_pos[1] + amount_of_move_y]
+    return [boat_pos, waypoint_relpos]
 
-initial_position = [0,0,2] # position = [0 = x, 1 = y, 2 = orientation]
-list_of_positions = [initial_position]
+initial_boat_position = [0,0] # position = [0 = x, 1 = y]
+initial_waypoint_position = [10,-1]
+list_of_positions = [[initial_boat_position,initial_waypoint_position]]
 for command in commands:
-    new_position = parse_command(command,list_of_positions[-1])
-    list_of_positions.append(new_position)
+    new_positions = move_waypoint_and_boat(command,list_of_positions[-1][0], list_of_positions[-1][1])
+    list_of_positions.append(new_positions)
 print(list_of_positions[-1])
-print(abs(list_of_positions[-1][0]) + abs(list_of_positions[-1][1]))
+print(abs(list_of_positions[-1][0][0]) + abs(list_of_positions[-1][0][1]))
