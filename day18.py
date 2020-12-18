e9a5f107fe9e9b972d105a1fc26cb0cbac794fd3
katiesteckles <katiesteckles@gmail.com>
2020-12-18 19:05:53 +0000

    Pseudo day 18

--- day18.py
+++ day18.py
@@ -1,4 +1,17 @@
 import copy
+import re
 with open("day18_input.txt", "r") as file:
     lines = file.readlines()
 
+def resolve_lr(input_string): # the input string should not contain brackets
+    total = 0
+    # work from left to right and resolve it: if it's a number, put it in a thingand, and then add/multiply the thingtrix
+    return total
+
+# something with brackets
+brackets = count(brackets)
+
+while brackets > 0:
+    # find a pair of brackets with no brackets inside (using a regex)
+    # replace the brackets and their contents with the resolve_lr of the contents of the bracket
+    # recount the brackets
