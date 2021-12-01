83732905783faee5dab37da3718a95e8af07aefc
katiesteckles <katiesteckles@gmail.com>
2021-12-01 19:07:23 +0000

    Day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,4 +1,26 @@
-with open("day01_input.txt", "r") as file:
+with open("2021/day01_input.txt", "r") as file:
     numbers = file.readlines()
 
-numbers_int = [int(x[:-1]) for x in numbers]
+nums_int = [int(x[:-1]) for x in numbers]
+
+# part 1
+
+incdecs = []
+for i in range(len(nums_int)):
+    if nums_int[i] > nums_int[i-1]:
+        incdecs.append(1)
+    else:
+        incdecs.append(0)
+
+totalincs = sum([x for x in incdecs])
+
+# part 2
+
+incdecs2 = []
+for i in range(len(numbers_int)-2):
+    if (nums_int[i+2]) > (nums_int[i-1]):
+        incdecs2.append(1)
+    else:
+        incdecs2.append(0)
+
+totalincs2 = sum([x for x in incdecs2])
