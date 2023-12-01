e7ae55620e200ca5dec58001c64a9da2ce504d12
katiesteckles <katiesteckles@gmail.com>
2023-12-01 09:10:01 +0000

    Day 1 refactoring

--- 2023/day01.py
+++ 2023/day01.py
@@ -9,13 +9,11 @@ digits = "0123456789"
 def extract_numbers(list):
     total = 0
     for item in list:
-        exploded_list = [*item]
-        print(exploded_list)
         numbers = []
-        for letter in exploded_list:
+        for letter in [*item]:
             if letter in digits:
-                numbers.append(letter)
-        total += (10 * int(numbers[0])) + int(numbers[-1])
+                numbers.append(int(letter))
+        total += (10 * numbers[0]) + numbers[-1]
     return total
 
 
@@ -27,15 +25,15 @@ extract_numbers(input)
 
 test_input_b = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
 
-digits_verbose = {"one": "1",
-                  "two": "2",
-                  "three": "3",
-                  "four": "4",
-                  "five": "5",
-                  "six": "6",
-                  "seven": "7",
-                  "eight": "8",
-                  "nine": "9"}
+digits_verbose = {"one": 1,
+                  "two": 2,
+                  "three": 3,
+                  "four": 4,
+                  "five": 5,
+                  "six": 6,
+                  "seven": 7,
+                  "eight": 8,
+                  "nine": 9}
 
 
 def extract_numbers_verbose(list):
@@ -47,14 +45,13 @@ def extract_numbers_verbose(list):
         while len(item) > 0:
             # treat the case where you're on the last character
             if len(item) == 1:
-                print("The length of " + item + " is 1")
                 if item in digits:
                     numbers.append(item)
                 break
             # create a window that expands until it finds something
             chunk = item[0:i]
             if chunk in digits:  # found a single-digit number
-                numbers.append(chunk)
+                numbers.append(int(chunk))
                 item = item[1:]
             elif chunk in digits_verbose:  # found a word number
                 numbers.append(digits_verbose[chunk])
@@ -66,7 +63,7 @@ def extract_numbers_verbose(list):
             else:  # didn't find either so just look for longer words
                 i += 1
         print(numbers)
-        total += (10 * int(numbers[0])) + int(numbers[-1])
+        total += (10 * numbers[0]) + numbers[-1]
     return total
 
 
